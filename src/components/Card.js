cb21be19b658dd6e65858eb7cf753fa462199f1f
髙山寛 <kanstephentakayama@gmail.com>
2024-09-24 23:53:10 +0900

    <ADD>ポケモンの画像と名前を表示

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Card = ({ pokemon }) => {
   return (
-    <div>Card</div>
+    <div className='card'>
+      <div className='cardImg'>
+        <img src={pokemon.sprites.front_default} alt='' />
+      </div>
+      <h3 className='cardName'>{pokemon.name}</h3>
+    </div>
   )
 }
 
